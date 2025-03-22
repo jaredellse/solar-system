2c931fb6aa656facded275bedadab5d17d36327e
Jared Ellse <jared@Jareds-Air.lan>
2025-03-21 21:14:25 -0700

    Update Vite config for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,24 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
-    },
-  },
-  publicDir: 'public',
+  base: '/solar-system/',
   build: {
+    outDir: 'dist',
     assetsDir: 'assets',
-    copyPublicDir: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          three: ['three'],
-          react: ['react', 'react-dom'],
-        }
-      }
-    }
-  },
-  assetsInclude: ['**/*.jpg', '**/*.png'],
-  base: '/solar-system/'
+    sourcemap: true
+  }
 })
